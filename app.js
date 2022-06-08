4f3284de9a39a7dc6024863f863afa167304701c
erosCarhuancho <eros.carhuancho@utec.edu.pe>
2022-06-07 21:32:43 -0500

    app2

--- app.js
+++ app.js
@@ -12,10 +12,11 @@ app.get('/fibonacci/:n', (req, res) => {
     const n = req.params.n;
     var num1 = 0;
     var num2 = 1;
+    var total = 0;
 	for (i=2; i<=n; i++) {
 		total = num2 + num1;
         num2 = total;
         num1 = num2;
 	}
-    res.json({factorial: total})
+    res.send(`Fibonacci de ${n} = ${total}`)
 })
