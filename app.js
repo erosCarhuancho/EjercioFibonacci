16bf8ad6bfe75eaf49d248477497738aa83d0fa9
erosCarhuancho <eros.carhuancho@utec.edu.pe>
2022-06-07 21:29:36 -0500

    app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.listen(3000, ()=>console.log("Servidor en puerto 3000"))
+
+app.get('/fibonacci/:n', (req, res) => {
+    const n = req.params.n;
+    var num1 = 0;
+    var num2 = 1;
+	for (i=2; i<=n; i++) {
+		total = num2 + num1;
+        num2 = total;
+        num1 = num2;
+	}
+    res.json({factorial: total})
+})
